a745e75b83f9d255f80d21e65aa1f5bdce951a6f
Nikita Butenko <nikita@butenko.au>
2023-08-27 18:48:16 +0800

    Autoplay on mouseover

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ addEventListener('scroll', () => {
     });
   }
 });
+
+document.querySelectorAll('.names .name').forEach(el => {
+  const video = el.querySelector('video');
+  const progress = el.querySelector('.progress');
+  el.addEventListener('mouseenter', () => video.play());
+  el.addEventListener('mouseleave', () => video.pause());
+  video.addEventListener('timeupdate', (e) => {
+    progress.style.width = `${Math.ceil(e.target.currentTime / e.target.duration * 100)}%`;
+  });
+})
+
